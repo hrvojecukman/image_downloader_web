a44f712567610fc77f293f035b6f1903dacce25d
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-10-19 08:59:06 +0200

    Added formats and updated libraries

--- lib/src/web_image_downloader.dart
+++ lib/src/web_image_downloader.dart
@@ -4,13 +4,41 @@ import 'package:flutter/painting.dart';
 import 'package:http/http.dart' as http;
 import 'package:universal_html/html.dart' as html;
 
-class WebImageDownloader {
+enum ImageType {
+  /// Animated Portable Network Graphics (APNG)
+  apng('image/apng'),
+
+  /// AV1 Image File Format (AVIF)
+  avif('image/avif'),
+
+  /// Graphics Interchange Format (GIF)
+  gif('image/gif'),
+
+  /// Joint Photographic Expert Group image (JPEG)
+  jpeg('image/jpeg'),
+
+  /// Portable Network Graphics (PNG)
+  png('image/png'),
+
+  /// Scalable Vector Graphics (SVG)
+  svg('image/svg+xml'),
+
+  /// Web Picture format (WEBP)
+  webp('image/webp');
+
+  const ImageType(this.format);
+
+  final String format;
+}
+
+abstract class WebImageDownloader {
   /// Download image from URL to user's device. It works only for Flutter web.
-  Future<void> downloadImageFromWeb(
+  static Future<void> downloadImageFromWeb(
     String url, {
     Map<String, String>? headers,
     double imageQuality = 0.95,
     String? name,
+    ImageType imageType = ImageType.png,
   }) async {
     final http.Response res = await http.get(
       Uri.parse(url),
@@ -29,10 +57,11 @@ class WebImageDownloader {
   }
 
   /// Download image from uInt8List to user device
-  Future<void> downloadImageFromUInt8List({
+  static Future<void> downloadImageFromUInt8List({
     required Uint8List uInt8List,
     double imageQuality = 0.95,
     String? name,
+    ImageType imageType = ImageType.png,
   }) async {
     final image = await decodeImageFromList(uInt8List);
 
@@ -55,13 +84,13 @@ class WebImageDownloader {
 
     final img = html.ImageElement();
 
-    img.src = "data:image/jpeg;base64,$base64";
+    img.src = "data:${imageType.format};base64,$base64";
 
     final html.ElementStream<html.Event> loadStream = img.onLoad;
 
     loadStream.listen((event) {
       ctx.drawImage(img, 0, 0);
-      final dataUrl = canvas.toDataUrl("image/jpeg", imageQuality);
+      final dataUrl = canvas.toDataUrl(imageType.format, imageQuality);
       final html.AnchorElement anchorElement =
           html.AnchorElement(href: dataUrl);
       anchorElement.download = name ?? dataUrl;
